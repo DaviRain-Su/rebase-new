205c288a8f0d4fe47f1ed028d64dd63f59c7cd4b
Davirain <davirain.yin@gmail.com>
2024-06-06 17:49:16 +0800

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <title>Web3Daily</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
